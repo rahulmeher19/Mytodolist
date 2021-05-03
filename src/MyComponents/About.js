8af8f0b45fdbfc51d875a998133c443226872567
rahulmeher-wscf <rahul@wscf.io>
2021-05-03 19:04:25 +0530

    Initial commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>Here i am trying to perform CRUD operation in TODO list </p>
+        </div>
+    )
+}
